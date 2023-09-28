9fcd7bfeca85f7b06aace86e50e2d173fbbc987f
EC2 Default User <ec2-user@ip-172-27-179-75.ap-southeast-1.compute.internal>
2023-09-28 05:48:42 +0000

    tmp1_1cczo8.mp4

--- stable_diffusion_xl.py
+++ stable_diffusion_xl.py
@@ -9,7 +9,8 @@ pipe.to("cuda")
 # pipe.enable_xformers_memory_efficient_attention()
 
 prompt = "An astronaut riding a green horse"
+negative_prompt = "nsfw,logo,text,badhandv4,EasyNegative,ng_deepnegative_v1_75t,rev2-badprompt,verybadimagenegative_v1.3,negative_hand-neg,mutated hands and fingers,poorly drawn face,extra limb,missing limb,disconnected limbs,malformed hands,ugly,FastNegativeV2,aid291,NegfeetV2"
 
-images = pipe(prompt=prompt).images[0]
+images = pipe(prompt=prompt, negative_prompt=negative_prompt).images[0]
 images
 images.save("/data1/liuda/tmp/xl_{}_P0.png".format(datetime.datetime.now().strftime('%Y%m%d_%H%M%S')))
